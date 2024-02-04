7660cfede8eb71050e8377652cabd2c8972f0414
CodeSandbox <support@codesandbox.io>
2024-02-04 17:28:13 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,74 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const phrases = [
+  "No 😤",
+  "Are you sure?",
+  "Really Sure😢",
+  "I'll be very sad😕",
+  "Taito Please🥺",
+  "sighhh 😭",
+  "You have no choice 😤",
+  "really?💔",
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const [text, setText] = useState("(1) New Message for Taito 📢");
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleYesClick() {
+    setYesPressed(true);
+    setText("Happy Valentine's Day Babe 😘");
+  }
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="valentine-container">
+      <div className="title">
+        <h1>{text}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {yesPressed ? (
+        <>
+          <img
+            src="https://media1.tenor.com/m/ZXBks2QSfdgAAAAd/cats-kittens.gif"
+            alt="cats-Kissing"
+          />
+          <div className="text">Love from Chi x</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="kiss"
+            src="https://media.tenor.com/cAO-Y2kE5CkAAAAi/tkthao219-bubududu.gif"
+            alt="panda"
+          />
+          <div className="text">Will you be my valentine 💜?</div>
+          <div className="both-Button">
+            <button
+              className="yesButton"
+              style={{
+                fontSize: yesButtonSize,
+                backgroundColor: "rgb(248, 229, 89)",
+              }}
+              onClick={handleYesClick}
+            >
+              Yes 👀
+            </button>
+            <button className="NoButton" style={{}} onClick={handleNoClick}>
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
